9d18b1ceb29700ad68da168f779d1d5669e1fcf4
Nico Orschel <nico.orschel@xebia.com>
2025-11-08 09:53:49 +0100

    Handle error from REST client request in GetTeamData function

--- internal/utils/general.go
+++ internal/utils/general.go
@@ -449,7 +449,10 @@ func (g *APIGetter) GetRepoRulesetsList(owner string, repo string, endCursor *st
 func (g *APIGetter) GetTeamData(ownerID int, teamID int) (*data.TeamInfo, error) {
 	url := fmt.Sprintf("organizations/%s/team/%s", strconv.Itoa(ownerID), strconv.Itoa(teamID))
 
-	resp, _ := g.restClient.Request("GET", url, nil)
+	resp, err := g.restClient.Request("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
 	defer func() {
 		if err := resp.Body.Close(); err != nil {
 			zap.S().Errorf("Error closing response body: %v", err)
